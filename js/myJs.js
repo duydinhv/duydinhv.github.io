43b79a685f649098565990d9ec4de071b8e6dc3b
duydinhv <53370450+duydinhv@users.noreply.github.com>
2020-03-16 21:15:14 +0700

    change something

--- js/myJs.js
+++ js/myJs.js
@@ -14,8 +14,8 @@ function firstQuestion(){
     
     $('.content').hide();
     Swal.fire({
-        title: 'Hê lu cậu!',
-        text: 'Tớ có điều này muốn hỏi cậu, cậu nhớ phải trả lời thật lòng nhé.',
+        title: 'Hê lu em!',
+        text: 'Anh có điều này muốn hỏi em, em nhớ phải trả lời thật lòng nhé.',
         imageUrl: 'img/cuteCat.jpg',
         imageWidth: 300,
         imageHeight: 300,
@@ -73,7 +73,7 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = " Tại vì cậu đẹp trai vl :< ";
+    var text = " Tại vì anh đẹp trai vl :< ";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
     var count = textVal.length;
@@ -96,7 +96,7 @@ $('#yes').click(function() {
     var audio = new Audio('sound/tick.mp3');
     audio.play();
     Swal.fire({
-        title: 'Nói cho tớ lí do cậu thích tớ đi =)))',
+        title: 'Nói cho anh lí do em thích anh đi =)))',
         html: true,
         width: 900,
         padding: '3em',
@@ -114,15 +114,15 @@ $('#yes').click(function() {
         cancelButtonColor: '#d33',
         confirmButtonColor: '#fe8a71',
         cancelButtonColor: '#f6cd61',
-        confirmButtonText: 'Gửi cho tớ <3'
+        confirmButtonText: 'Gửi cho anh <3'
     }).then((result) => {
         if (result.value) {
             Swal.fire({
                 width: 900,
                 confirmButtonText: 'Okiiiii lun <3',
                 background: '#fff url("img/iput-bg.jpg")',
-                title: 'Tớ biết mà ^^ Yêu cậu 3000',
-                text: "Tối nay tớ qua đón cậu đi chơi nhé :v Còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào",
+                title: 'Anh biết mà ^^ Yêu em 3000',
+                text: "Tối nay anh qua đón em đi chơi nhé :v Còn giờ thì chờ gì nữa mà không inbox cho anh đi nào",
                 confirmButtonColor: '#83d0c9',
                 onClose: () => {
                     window.location = 'http://fb.com';
